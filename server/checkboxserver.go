1b8e910f7b7d859210748e6877a0c8d5810e8886
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2025-04-10 20:27:30 -0600

    works

--- server/checkboxserver.go
+++ server/checkboxserver.go
@@ -1,233 +1,229 @@
-package checkboxserver
+package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"time"
-	"math"
 
 	"github.com/gorilla/websocket"
-	// "github.com/minio/sha256-simd"
-
-	"github.com/PeterHindes/bitarrayutils/fileutils"
-	// "github.com/PeterHindes/bitarrayutils/compression/brle"
-	// "github.com/PeterHindes/bitarrayutils/compression/rle"
 )
 
-// Message type enums
-// we have change, checkinhash, and versioncheck
-// which are encoded as 0, 1, and 2 respectively using 4 bits
-const (
-	Change = 0
-	CheckInHash = 1
-	VersionCheck = 2
-)
+type Settings struct {
+	BoardSize int `json:"boardSize"`
+}
 
-// Handle WebSocket connections
-func handleWebSocket(w http.ResponseWriter, r *http.Request, bitArray *[1000000]bool) {
-	// Allow all origins
-	upgrader := websocket.Upgrader{
+type Message struct {
+	Type  string `json:"type,omitempty"`
+	ID    int    `json:"id,omitempty"`
+	State bool   `json:"state,omitempty"`
+}
+
+var (
+	settings = Settings{
+		BoardSize: 1000000, // Default board size
+	}
+	bitArray []bool
+	clients  = make(map[*websocket.Conn]bool)
+	upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			return true
 		},
 	}
+)
 
-	// Upgrade HTTP connection to WebSocket
-	conn, err := upgrader.Upgrade(w, r, nil)
+func loadSettings() error {
+	data, err := os.ReadFile("settings.json")
 	if err != nil {
-		log.Println("Failed to upgrade connection to WebSocket:", err)
-		return
-	}
-	defer conn.Close()
-
-	// Right after it opens send the full bitArray, the client will simply verify that the blob is the correct length because otherwise it is an rle encoded blob
-	err = sendBitArray(conn, bitArray[:])
-	if err != nil {
-		log.Println("Failed to send bitArray:", err)
-		return
-	}
-
-	// Send check the channel for changes in the bitArray and send them to the client
-	go func() {
-		for {
-			time.Sleep(200 * time.Millisecond)
-			// Wait for the update of the encoded bitArray
-			<-encodedArrayMutex
-			log.Println("Sending encoded bitArray...")
-			err := sendBitArray(conn, encodedBitArray)
+		if os.IsNotExist(err) {
+			// Create default settings file if it doesn't exist
+			defaultSettings := Settings{BoardSize: 1000000}
+			data, err := json.MarshalIndent(defaultSettings, "", "    ")
 			if err != nil {
-				log.Println("Failed to send bitArray:", err)
-				break
+				return fmt.Errorf("error creating default settings: %v", err)
 			}
-			encodedArrayMutex <- true
-		}
-	}()
-
-	// Read messages from WebSocket, they will all be byte arrays
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Failed to read message from WebSocket:", err)
-			break
-		}
-
-		// Figure out what type of message we have
-
-		// First convert what we recived to a bitArray
-		byteArray := message
-		bitArray := make([]bool, len(byteArray)*8)
-		for i := 0; i < len(byteArray); i++ {
-			for j := 0; j < 8; j++ {
-				bitArray[i*8+j] = byteArray[i]&(1<<uint8(7-j)) != 0
+			if err := os.WriteFile("settings.json", data, 0644); err != nil {
+				return fmt.Errorf("error writing default settings: %v", err)
 			}
+			return nil
 		}
-
-		// Check the message type in first 4 bits
-		// use a slice to get the first 4 bits
-		messageType := bitArray[:4]
-
-		// convert message type to an integer
-		// we will use this to determine what to do with the message
-		messageTypeInt := 0
-		for i := 0; i < 4; i++ {
-			if messageType[i] {
-				messageTypeInt |= 1 << uint8(3-i)
-			}
-		}
-
-		log.Println("Message type:", messageType) // TODO make this use the enums to print the actual message type
-
-		// Handle the message
-		switch messageTypeInt {
-			case Change: handleChange(bitArray[4:]) // Changes will be in 20 bits indicating the position of the change
-			case CheckInHash: handleCheckInHash(bitArray[4:]) // CheckInHash will be in 256 bits as a sha256 hash
-			case VersionCheck: handleVersionCheck(bitArray[4:]) // VersionCheck will be in 32 bits as an integer
-		}
+		return fmt.Errorf("error reading settings: %v", err)
 	}
 
-}
-func sendBitArray(conn *websocket.Conn, bitArray []bool) error {
-	// Encode the bitArray into a binary message without json
-	// Calculate the number of bytes needed to store the bit array
-	byteArraySize := int(math.Ceil(float64(len(bitArray)) / 8))
-	byteArray := make([]byte, byteArraySize)
-	for i := 0; i < len(bitArray); i++ {
-		if bitArray[i] {
-			byteArray[i/8] |= 1 << uint8(7-i%8)
-		}
+	if err := json.Unmarshal(data, &settings); err != nil {
+		return fmt.Errorf("error parsing settings: %v", err)
 	}
 
-	// Handle the remaining bits TODO: Check if ai did a good job here
-	remainingBits := len(byteArray)*8 - len(bitArray)
-	if remainingBits > 0 {
-		byteArray[len(byteArray)-1] >>= uint8(remainingBits)
-		byteArray[len(byteArray)-1] <<= uint8(remainingBits)
-	}
-
-	// Send the bitArray
-	err := conn.WriteMessage(websocket.BinaryMessage, byteArray)
-	if err != nil {
-		return err
-	}
-
+	// Initialize bitArray with configured size
+	bitArray = make([]bool, settings.BoardSize)
 	return nil
 }
 
-func handleChange(bitData []bool) {
-	// decode the index from the first 20 bits
-	index := 0
-	for i := 0; i < 20; i++ {
-		if bitData[i] {
-			index |= 1 << uint8(19-i)
+func sendBitArrayToClient(conn *websocket.Conn) error {
+	bytes := make([]byte, (settings.BoardSize+7)/8) // Calculate the number of bytes needed
+	for i := 0; i < len(bitArray); i++ {
+		if bitArray[i] {
+			bytes[i/8] |= 1 << uint(7-(i%8))
 		}
 	}
-
-	// decode the value from the last bit
-	value := bitData[20]
-
-	bitArray[index] = value
+	return conn.WriteMessage(websocket.BinaryMessage, bytes)
 }
 
-var bitArray [1000000]bool
-
-// sliding window that can hold 5*5 256bit hashes
-var validHashes = make([][32]byte, 25)
-var validHashesIndex = 0
-var statistics = make(map[string]int)
-var statisticsMutex = make(chan bool, 1)
-
-func main() {
-
-	// Initialize the bitArray with values from a file
-	err := fileutils.loadBinaryFile(&bitArray, "bitArray.bin")
-	if err != nil {
-		fmt.Println("Failed to load bitArray from file:", err)
-		os.Exit(1)
+func broadcastToAll() {
+	bytes := make([]byte, (settings.BoardSize+7)/8)
+	for i := 0; i < len(bitArray); i++ {
+		if bitArray[i] {
+			bytes[i/8] |= 1 << uint(7-(i%8))
+		}
 	}
 
-	// Before stoping save the bitArray to a file when the program is stopped with ctrl+c
-	// Before stopping, save the bitArray to a file when the program is stopped with ctrl+c
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		<-c
+	for client := range clients {
+		err := client.WriteMessage(websocket.BinaryMessage, bytes)
+		if err != nil {
+			log.Printf("Error broadcasting to client: %v", err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
 
-		// Save the bitArray to a file
-		saveBinaryFile(bitArray[:], "bitArray.bin")
-		log.Println("BitArray saved to bitArray.bin")
+func broadcastChange(sender *websocket.Conn, id int, state bool) {
+	// Create 5-byte message: type (1 byte) + index (3 bytes) + state (1 byte)
+	msg := make([]byte, 5)
+	msg[0] = 0 // type 0 = checkbox change
+	msg[1] = byte(id >> 16)
+	msg[2] = byte(id >> 8)
+	msg[3] = byte(id)
+	if state {
+		msg[4] = 1
+	}
 
-		os.Exit(0)
-	}()
-	// Also save every 5 seconds
-	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-			saveBinaryFile(bitArray[:], "bitArray.bin")
-			log.Println("BitArray saved to bitArray.bin")
-			// Also save the statistics to a json file
-			statisticsMutex <- true
-			file, err := os.Create("statistics.json")
-			if err != nil {
-				log.Println("Failed to create statistics file:", err)
-			} else {
-				encoder := json.NewEncoder(file)
-				err = encoder.Encode(statistics)
-				if err != nil {
-					log.Println("Failed to encode statistics:", err)
-				}
-				file.Close()
+	for client := range clients {
+		if client != sender {
+			if err := client.WriteMessage(websocket.BinaryMessage, msg); err != nil {
+				log.Printf("Error broadcasting to client: %v", err)
+				client.Close()
+				delete(clients, client)
 			}
 		}
+	}
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error upgrading to WebSocket: %v", err)
+		return
+	}
+
+	// Register the client
+	clients[conn] = true
+	defer func() {
+		delete(clients, conn)
+		conn.Close()
 	}()
 
-	// Every 200 milliseconds, update the encoded bitArray
+	// Send board size as 4 bytes
+	sizeMsg := make([]byte, 5)
+	sizeMsg[0] = 1 // type 1 = board size
+	sizeMsg[1] = byte(settings.BoardSize >> 24)
+	sizeMsg[2] = byte(settings.BoardSize >> 16)
+	sizeMsg[3] = byte(settings.BoardSize >> 8)
+	sizeMsg[4] = byte(settings.BoardSize)
+	if err := conn.WriteMessage(websocket.BinaryMessage, sizeMsg); err != nil {
+		log.Printf("Error sending board size: %v", err)
+		return
+	}
+
+	// Send initial board state
+	if err := sendBitArrayToClient(conn); err != nil {
+		log.Printf("Error sending initial state: %v", err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		if len(message) == 5 && message[0] == 0 { // checkbox change message
+			id := int(message[1])<<16 | int(message[2])<<8 | int(message[3])
+			state := message[4] != 0
+
+			if id >= 0 && id < settings.BoardSize {
+				bitArray[id] = state
+				log.Printf("Checkbox %d set to %v", id, state)
+				broadcastChange(conn, id, state)
+			}
+		}
+	}
+}
+
+func saveToDisk() {
+	bytes := make([]byte, (settings.BoardSize+7)/8)
+	for i := 0; i < len(bitArray); i++ {
+		if bitArray[i] {
+			bytes[i/8] |= 1 << uint(7-(i%8))
+		}
+	}
+	if err := os.WriteFile("bitArray.bin", bytes, 0644); err != nil {
+		log.Printf("Error saving to disk: %v", err)
+	} else {
+		log.Printf("Board state saved to disk")
+	}
+}
+
+func loadFromDisk() {
+	bytes, err := os.ReadFile("bitArray.bin")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Printf("Error loading from disk: %v", err)
+		}
+		return
+	}
+
+	maxBits := len(bytes) * 8
+	if maxBits > settings.BoardSize {
+		maxBits = settings.BoardSize
+	}
+
+	for i := 0; i < maxBits && i < len(bitArray); i++ {
+		bitArray[i] = (bytes[i/8] & (1 << uint(7-(i%8)))) != 0
+	}
+}
+
+func main() {
+	// Load settings
+	if err := loadSettings(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load initial state
+	loadFromDisk()
+
+	// Start periodic save and broadcast
 	go func() {
-		for {
-			time.Sleep(200 * time.Millisecond)
-			updateEncodedBitArray()
+		ticker := time.NewTicker(10 * time.Second)
+		for range ticker.C {
+			saveToDisk()
+			broadcastToAll()
 		}
 	}()
 
+	// Setup HTTP server
+	http.HandleFunc("/ws", handleWebSocket)
+	http.Handle("/", http.FileServer(http.Dir("static")))
 
-	// Register WebSocket handler
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		handleWebSocket(w, r, &bitArray)
-	})
-
-	// Serve static files
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
-
-	// Start server
 	addr := ":1335"
-	log.Println("Starting server on", addr)
-	errsrv := http.ListenAndServe(addr, nil)
-	if errsrv != nil {
-		log.Fatal("Failed to start server:", errsrv)
-	}
+	log.Printf("Server starting. Access at:\n")
+	log.Printf("    http://localhost%s\n", addr)
+	log.Printf("Board size: %d\n", settings.BoardSize)
 
-	
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
--- server/go.mod
+++ server/go.mod
@@ -2,7 +2,4 @@ module checkbox_server
 
 go 1.23.0
 
-require (
-	github.com/PeterHindes/bitarrayutils/fileutils v0.0.0-20240826235957-b1c624b79c83
-	github.com/gorilla/websocket v1.5.3
-)
+require github.com/gorilla/websocket v1.5.3
